23d9ca9d4a4b41e1809ba4f2accedc37a60b9ac8
ericmbouwe <ericmbouwe@yahoo.fr>
2021-09-19 17:44:51 +0100

    change script colors

--- RUBY sample/review-dashboard.rb
+++ RUBY sample/review-dashboard.rb
@@ -8,8 +8,8 @@ driver.navigate.to 'https://dashboard.microverse.org/review_requests'
 EMAIL =  ARGV[0] || 'ericmbouwe@yahoo.fr'
 PASSWORD =  ARGV[1] || 'Naruto00'
 
-REFRESH_TIMER = 2000 #-> 17 seconds
-LOGGING_TIMER = 5
+REFRESH_TIMER = 20 #-> 20 seconds
+LOGGING_TIMER = 5  #-> It depends of your internet speed
 
 sleep(1)
 
@@ -23,25 +23,23 @@ sleep(LOGGING_TIMER)
 element = driver.find_element(:link_text, 'Review Requests 2.0')
 element.click
 
-sleep(1)
-
 i = 1
 loop do
 
     claim_btns = driver.find_elements(:name, 'claim')
     # p claim_btns
 
-    # sleep(1)
+    sleep(1)
     puts "scrapping since #{i} seconds..."
 
     if i%REFRESH_TIMER == 0
         driver.navigate.refresh
-        puts '...Fast Page Refresh...'.colorize( :background => :blue)
+        puts '......Fast Page Refresh......'.colorize(:black).colorize(background: :blue)
     end
 
     if !claim_btns.empty?
         if claim_btns[0].displayed?
-            puts 'Claimed Code Review!!!'.colorize(:background => :green)
+            puts 'Claimed Code Review!!!'.colorize(:black).colorize(background: :green)
             claim_btns[0].click
             break
         end
