79a9af249995b1dd080488af253226d2a06e2eca
ericmbouwe <ericmbouwe@yahoo.fr>
2021-09-19 15:56:53 +0100

    refactor loop with no delay between each iteration

--- RUBY sample/review-dashboard.rb
+++ RUBY sample/review-dashboard.rb
@@ -8,8 +8,10 @@ driver.navigate.to 'https://dashboard.microverse.org/review_requests'
 EMAIL =  ARGV[0] || 'ericmbouwe@yahoo.fr'
 PASSWORD =  ARGV[1] || 'Naruto00'
 
-REFRESH_TIMER = 10
+REFRESH_TIMER = 1000
 LOGGING_TIMER = 5
+
+sleep(1)
 
 driver.find_element(:name, 'email').send_keys(EMAIL)
 driver.find_element(:name, 'password').send_keys(PASSWORD)
@@ -21,13 +23,14 @@ sleep(LOGGING_TIMER)
 element = driver.find_element(:link_text, 'Review Requests 2.0')
 element.click
 
+sleep(1)
+
 i = 1
 loop do
 
     claim_btns = driver.find_elements(:name, 'claim')
     # p claim_btns
 
-    sleep(1)
     puts "scrapping since #{i} seconds..."
 
     if i%REFRESH_TIMER == 0
